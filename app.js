45852f5d0ab4e1b718bfa5f3b109680145b652ef
afdmandreimontaniel <227302279+afdmandreimontaniel-sudo@users.noreply.github.com>
2026-02-22 12:27:33 +0800

    added files sa services and edited server.js at app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('CampusOne HRIS Auth Server Running');
+// health check
+app.get("/", (req, res) => {
+  res.send("CampusOne HRIS Auth Server Running");
 });
+
+// mount routes
+app.use("/auth", authRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// test route
-app.get('/', (req, res) => {
-    res.send('CampusOne HRIS Auth Server Running');
-});
+require("dotenv").config();
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
